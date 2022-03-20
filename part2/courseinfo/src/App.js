89943659b538005ae2c7f77927c02b7650811fd8
popovmik-arcada <popovmik@arcada.fi>
2022-03-20 20:21:24 +0200

    Exercises 2.1 - 2.3

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,56 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </>
+
+const Course = ({ course }) => {
+  const sum = course.parts.map(part => part.exercises).reduce(
+    (previousValue, currentValue) => previousValue + currentValue)
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={sum} />
+    </>
+  )
+}
+  
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
