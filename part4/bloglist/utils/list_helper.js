2103d22dc39dda1b2e849438b3206e775d46653d
popovmik-arcada <popovmik@arcada.fi>
2022-06-03 20:01:02 +0300

    Exc 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let likes = 0
+
+  blogs.forEach(element => {
+    likes += element.likes
+  })
+
+  if (isNaN(likes)) {
+    return 0 
+  }
+
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
