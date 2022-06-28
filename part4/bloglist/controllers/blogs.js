d113d651c0eb4ce90a4cff10936e11320c8b52c6
popovmik-arcada <popovmik@arcada.fi>
2022-06-28 17:19:36 +0300

    Exercise 4.13 + fixes

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -11,12 +11,21 @@ blogsRouter.post('/', async (request, response) => {
 
   if (!body.title || !body.url) {
     response.status(400).end()
+  } else {
+    const blog = new Blog(body)
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
   }
+})
 
-  const blog = new Blog(body)
-
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+blogsRouter.delete('/:id', async (request, response) => {
+  const result = await Blog.findByIdAndRemove(request.params.id)
+  console.log(result)
+  if (result) {
+    response.status(204).end()
+  } else {
+    response.status(404).end()
+  }
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -14,90 +14,120 @@ beforeEach(async () => {
     await blogObject.save()
   }
 })
+describe('getting existing blogs', () => {
+  test('blogs are returned in the JSON format', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('blog posts are returned in the JSON format', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+  test('unique identifier is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+
+    response.body.forEach(blog => {
+      expect(blog.id).toBeDefined()
+    })
+  })
 })
+describe('creating a new blog', () => {
+  test('blogs can be created with POST request', async () => {
+    const newBlog = {
+      title: "blog added from the test",
+      author: "default author",
+      url: "https://example.com/",
+      likes: 2
+    }
 
-test('unique identifier is named "id"', async () => {
-  const response = await api.get('/api/blogs')
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  response.body.forEach(blog => {
-    expect(blog.id).toBeDefined()
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain('blog added from the test')
+  })
+
+  test('missing likes results in 0', async () => {
+    const newBlog = {
+      title: "blog with missing likes",
+      author: "default author",
+      url: "https://example.com/"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const indexOfTest = blogsAtEnd.findIndex(blog => blog.title === 'blog with missing likes')
+    expect(blogsAtEnd[indexOfTest].likes).toEqual(0)
+  })
+
+  test('400 Bad Request response to missing title and/or url', async () => {
+    let newBlog = {
+      author: "default author"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    newBlog = {
+      author: "default author",
+      url: "https://example.com/"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    newBlog = {
+      title: "blog with no url",
+      author: "default author"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
   })
 })
 
-test('blogs can be created with POST request', async () => {
-  const newBlog = {
-    title: "blog added from the test",
-    author: "default author",
-    url: "https://example.com/",
-    likes: 2
-  }
+describe('deleting blogs', () => {
+  test('deleting a single blog by id', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToBeDeleted = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .delete(`/api/blogs/${blogToBeDeleted.id}`)
+      .expect(204)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const blogsAtEnd = await helper.blogsInDb()
 
-  const titles = blogsAtEnd.map(blog => blog.title)
-  expect(titles).toContain('blog added from the test')
-})
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
 
-test('missing likes results in 0', async () => {
-  const newBlog = {
-    title: "blog with missing likes",
-    author: "default author",
-    url: "https://example.com/"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const indexOfTest = blogsAtEnd.findIndex(blog => blog.title === 'blog with missing likes')
-  expect(blogsAtEnd[indexOfTest].likes).toEqual(0)
-})
-
-test('400 Bad Request response to missing title and/or url', async () => {
-  let newBlog = {
-    author: "default author"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  newBlog = {
-    author: "default author",
-    url: "https://example.com/"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  newBlog = {
-    title: "blog with no url",
-    author: "default author"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    const contents = blogsAtEnd.map(blog => blog.title)
+    expect(contents).not.toContain(blogToBeDeleted.title)
+  })
+  test('deleting a blog with non-existing id', async () => {
+    await api
+      .delete('/api/blogs/5a422aa71b54a676234d17f3')
+      .expect(404)
+  })
+  test('deleting a blog with id in a wrong format', async () => {
+    await api
+      .delete('/api/blogs/123')
+      .expect(500)
+  })
 })
 
 afterAll(() => {
