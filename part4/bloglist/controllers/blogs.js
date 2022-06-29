b23c26bd7bf1708611b911c97a1d839e843ba26b
popovmik-arcada <popovmik@arcada.fi>
2022-06-29 11:58:10 +0300

    Exercise 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -12,8 +14,19 @@ blogsRouter.post('/', async (request, response) => {
   if (!body.title || !body.url) {
     response.status(400).end()
   } else {
-    const blog = new Blog(body)
+    const user = await User.findOne({})
+
+    const blog = new Blog({
+      title: body.title,
+      url: body.url,
+      likes: body.likes,
+      author: body.author,
+      user: user
+    })
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    
     response.status(201).json(savedBlog)
   }
 })
