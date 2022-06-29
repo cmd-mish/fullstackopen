072394bbad5e8e0d1789ca0c8d2fa9b1d3b67512
popovmik-arcada <popovmik@arcada.fi>
2022-06-29 12:14:45 +0300

    Exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
@@ -14,5 +15,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
