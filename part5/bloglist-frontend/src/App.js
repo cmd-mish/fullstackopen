868c7d266342f5b45317c16b92268e5ac1fecd20
popovmik-arcada <popovmik@arcada.fi>
2022-06-29 23:54:11 +0300

    Exercise 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +16,57 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  })
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      
+      window.localStorage.setItem(
+        'loggedInUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null ?
+       <div>
+        <h2>log in to application</h2>
+        <LoginForm 
+          handleLogin={handleLogin}
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+        />
+      </div> :
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} is logged in</p>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+      }
     </div>
   )
 }
---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,25 @@
+const LoginForm = (props) => (
+  <form onSubmit={props.handleLogin}>
+    <div>
+      username
+      <input
+        type="text"
+        value={props.username}
+        name="Username"
+        onChange={({ target }) => props.setUsername(target.value)}
+      />
+    </div>
+    <div>
+      password
+      <input
+        type="password"
+        value={props.password}
+        name="Password"
+        onChange={({ target }) => props.setPassword(target.value)}
+      />
+    </div>
+    <button type="submit">login</button>
+  </form>
+)
+
+export default LoginForm
