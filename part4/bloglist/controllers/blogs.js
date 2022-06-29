b881f64e7b7b661ce1db91e930355fded7ee87cb
popovmik-arcada <popovmik@arcada.fi>
2022-06-29 12:36:47 +0300

    Exercise 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const auth = request.get('authorization')
+  if (auth && auth.toLowerCase().startsWith('bearer ')) {
+    return auth.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -10,18 +19,23 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'missing or invalid token'})
+  }
 
   if (!body.title || !body.url) {
     response.status(400).end()
   } else {
-    const user = await User.findOne({})
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
       url: body.url,
       likes: body.likes,
       author: body.author,
-      user: user
+      user: user.id
     })
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
