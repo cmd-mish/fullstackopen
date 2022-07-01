269b598d5436f0aa25de56e2cc916469d8d50852
popovmik-arcada <popovmik@arcada.fi>
2022-07-01 12:50:49 +0300

    Exercise 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -111,9 +111,14 @@ const App = () => {
             />
           </Togglable>
         </div>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} changeLikes={changeLikes} />
-        )}
+        {blogs
+          .sort((a, b) =>  
+            b.likes - a.likes
+          )
+          .map(blog =>
+            <Blog key={blog.id} blog={blog} changeLikes={changeLikes} />
+          )
+        }
       </div>
       }
     </div>
