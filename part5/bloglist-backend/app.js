5ffbd1b9d26b9451e8bc8f403d83e8215bc49d87
popovmik-arcada <popovmik@arcada.fi>
2022-07-04 14:42:44 +0300

    Exercise 5.17

--- part5/bloglist-backend/app.js
+++ part5/bloglist-backend/app.js
@@ -21,6 +21,11 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/test')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(errorHandler)
 
 module.exports = app
