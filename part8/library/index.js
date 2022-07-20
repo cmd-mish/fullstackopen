3a5ec3ca8c1715c7da48ca3074b7ae95871c8e3b
cmd-mish <popov.mikhail.al@gmail.com>
2022-07-21 00:55:27 +0300

    Exercise 8.7

--- part8/library/index.js
+++ part8/library/index.js
@@ -119,6 +119,10 @@ const typeDefs = gql`
       published: Int
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -161,6 +165,14 @@ const resolvers = {
 
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const existingAuthor = authors.find(author => author.name === args.name)
+      if (!existingAuthor) return null
+
+      const updatedAuthor = { ...existingAuthor, born: args.setBornTo }
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
