4439902634c6f283fec510152bc0b5148bf559cd
cmd-mish <popov.mikhail.al@gmail.com>
2022-08-01 01:08:10 +0300

    Exercise 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('ping pong');
-  res.send('pong');
+  res.send('ping');
 });
 
 app.listen(PORT, () => {
